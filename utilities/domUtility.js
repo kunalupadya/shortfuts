6a8b31010dd9019294ddb443bd525081af0aebc6
Joe Martella <joem@microsoft.com>
2017-12-07 16:06:21 -0800

    Fixes "list item" shortcut (poorly because I'm at work)

--- utilities/domUtility.js
+++ utilities/domUtility.js
@@ -63,16 +63,41 @@ function clickDetailsPanelButton(buttonLabel) {
     commonUtility.log(`Attempting to press "${buttonLabel}" button...`);
 
     try {
+        // Expand "List on Transfer Market" section.
+        if (buttonLabel === 'List Item') {
+            const buttons = document.getElementsByTagName('button');
+            for (button of buttons) {
+                if (button && button.innerHTML.indexOf('List on Transfer Market') > -1) {
+                    _tapElement(button);
+
+                    setTimeout(() => {
+                        // Get buttons in the details panel.
+                        const detailsPanel = document.getElementsByClassName('DetailPanel')[0];
+                        const detailsPanelButtons = detailsPanel.getElementsByTagName('button');
+                        const buttonArray = Array.from(detailsPanelButtons);
+
+                        // Find target button by searching by label.
+                        const _button = buttonArray.filter((__button) => __button.innerHTML.indexOf(buttonLabel) > -1 && __button.style.display !== 'none')[0];
+
+                        // Click target button.
+                        _tapElement(_button);
+                    }, commonUtility.getRandomWait());
+
+                    return;
+                }
+            }
+        }
+
         // Get buttons in the details panel.
         const detailsPanel = document.getElementsByClassName('DetailPanel')[0];
         const detailsPanelButtons = detailsPanel.getElementsByTagName('button');
         const buttonArray = Array.from(detailsPanelButtons);
 
         // Find target button by searching by label.
-        const button = buttonArray.filter((button) => button.innerText.indexOf(buttonLabel) > -1 && button.style.display !== 'none')[0];
+        const _button = buttonArray.filter((foo) => foo.innerHTML.indexOf(buttonLabel) > -1 && foo.style.display !== 'none')[0];
 
         // Click target button.
-        _tapElement(button);
+        _tapElement(_button);
     } catch (error) {
         throw `Unable to locate the "${buttonLabel}" button.`;
     }
